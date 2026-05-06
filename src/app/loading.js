22fc7ee89d8096ad4cf05d6e88a1a50ccce3b8c1
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-05-06 10:09:52 +0600

    added the loading spinner

---
+++ src/app/loading.js
@@ -0,0 +1,5 @@
+import { HashLoader } from "react-spinners";
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return<div className="text-center text-4xl h-[40vh] mx-auto text-pink-500"><HashLoader/></div> 
+}
